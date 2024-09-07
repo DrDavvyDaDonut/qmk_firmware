af27753fdec0352438e2cd61dc6dba7a9366f763
DrDavvyDaDonut <elewisc@gmail.com>
2024-09-06 20:23:07 -0400

    no more gitignore messages

---
+++ keyboards/keycapsss/3w6_2040/keymaps/joystick/config.h
@@ -0,0 +1,58 @@
+#pragma once
+
+//  easier bootloader access
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
+// 1000hz Polling Rate
+#define USB_POLLING_INTERVAL_MS 1
+#define FORCE_NKRO
+
+#define PIMORONI_TRACKBALL_SCALE 1
+
+#define EE_HANDS
+
+#define TAPPING_TERM 250
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define QUICK_TAP_TERM 120
+#define QUICK_TAP_TERM_PER_KEY
+
+//  constant mouse keys
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL        //  momentary mouse
+
+#define MK_C_OFFSET_0   	    1   //  speeds
+#define MK_C_OFFSET_UNMOD     3   //  default
+#define MK_C_OFFSET_1   	    8
+#define MK_C_OFFSET_2   	    16
+
+#define MK_C_INTERVAL_0 	    8   //  timings
+#define MK_C_INTERVAL_1 	    8
+#define MK_C_INTERVAL_UNMOD   8
+#define MK_C_INTERVAL_2 	    8
+
+#define MK_W_OFFSET_0   	    1   //  speeds
+#define MK_W_OFFSET_1   	    1
+#define MK_W_OFFSET_UNMOD     1
+#define MK_W_OFFSET_2   	    1
+
+#define MK_W_INTERVAL_0 	    300 //  timings
+#define MK_W_INTERVAL_1 	    200
+#define MK_W_INTERVAL_UNMOD   100 //  default
+#define MK_W_INTERVAL_2       30
+
+#define BOOTMAGIC_ROW_RIGHT 4
+#define BOOTMAGIC_COLUMN_RIGHT 0
+
+#define RGBLIGHT_SLEEP
+
+// Joystick Button Count
+#define JOYSTICK_BUTTON_COUNT 17
+
+// Joystick Axes Count
+#define JOYSTICK_AXIS_COUNT 4
+
+// Joystick Axes Resolution
+#define JOYSTICK_AXIS_RESOLUTION 8
--- keyboards/keycapsss/3w6_2040/keymaps/joystick/keymap.c
+++ keyboards/keycapsss/3w6_2040/keymaps/joystick/keymap.c
@@ -4,10 +4,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "g/keymap_combo.h"
-#ifdef CONSOLE_ENABLE
-  #include "print.h"
-#endif
 
 void socdCleaner(uint8_t * totalState, uint8_t bit, bool on, uint16_t keyOne, uint16_t keyTwo);
 
@@ -22,6 +18,13 @@ enum layers {
   _MOUSE,
   _MINECRAFT,
   _FIGHTER
+};
+
+joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
+  JOYSTICK_AXIS_VIRTUAL,
+  JOYSTICK_AXIS_VIRTUAL,
+  JOYSTICK_AXIS_VIRTUAL,
+  JOYSTICK_AXIS_VIRTUAL
 };
 
 enum keycodes {
@@ -54,7 +57,21 @@ enum keycodes {
   AR_U,
 
   //  scrollArrowToggle
-  toggle
+  toggle,
+
+  LS_UPUP,  //  left stick
+  LS_DOWN,
+  LS_LEFT,
+  LS_RGHT,
+  RS_UPUP,  //  right stick
+  RS_DOWN,
+  RS_LEFT,
+  RS_RGHT,
+
+  DP_UPUP,  //  d pad
+  DP_DOWN,
+  DP_LEFT,
+  DP_RGHT,
 };
 
 #define ctrlA     LCTL_T(KC_A)
@@ -83,6 +100,19 @@ enum keycodes {
 #define GAMEPAD   TO(_GAMEPAD)
 #define FIGHTER   TO(_FIGHTER)
 
+#define	BC_DOWN   JS_0   //  button cluster
+#define	BC_RGHT   JS_1
+#define	BC_LEFT   JS_2
+#define	BC_UPUP   JS_3
+#define	L1_PRSS   JS_4   //  bumpers
+#define	R1_PRSS   JS_5
+#define	L2_PRSS   JS_6   //  triggers
+#define	R2_PRSS   JS_7
+#define	GC_SLCT   JS_8   //  select
+#define	GC_STRT   JS_9   //  start
+#define	LS_PRSS   JS_10  //  sticks
+#define	RS_PRSS   JS_11
+
 bool scrollOrArrow = false;
 
 uint8_t highest_layer = _BASE;
@@ -90,7 +120,29 @@ uint8_t highest_layer = _BASE;
 uint8_t bllSpd = 1;
 uint8_t mouseSOCD = 0;
 uint8_t gameSOCD = 0;
+bool LSU = false;
+bool LSD = false;
+bool LSL = false;
+bool LSR = false;
+bool RSU = false;
+bool RSD = false;
+bool RSL = false;
+bool RSR = false;
+bool DPU = false;
+bool DPD = false;
+bool DPL = false;
+bool DPR = false;
 
+int8_t leftValue = 127;
+int8_t leftDiag = 90;
+int8_t rightValue = 127;
+int8_t rightDiag = 90;
+
+//  i used this because i couldn't get joystick_read_axis() to work
+int8_t leftHor = 0;
+int8_t leftVer = 0;
+int8_t rightHor = 0;
+int8_t rightVer = 0;
 //  on startup
 void keyboard_post_init_user(void){
   combo_disable();
@@ -119,6 +171,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   }
 
   return state;
+}
+
+void leftStick(void){
+  joystick_set_axis(0, leftHor * ((leftVer == 0) ? leftValue : leftDiag));
+  joystick_set_axis(1, leftVer * ((leftHor == 0) ? leftValue : leftDiag));
+}
+
+void rightStick(void){
+  joystick_set_axis(2, rightHor * ((rightVer == 0) ? rightValue : rightDiag));
+  joystick_set_axis(3, rightVer * ((rightHor == 0) ? rightValue : rightDiag));
 }
 
 //  process keycode
@@ -228,6 +290,110 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case toggle:
       if (record->event.pressed){
         scrollOrArrow = ! scrollOrArrow;
+      }
+      return false;
+      break;
+    case LS_UPUP:
+      LSU = record->event.pressed ? true : false;
+      leftVer = record->event.pressed ? -1  : (LSD ? 1 : 0);
+      leftStick();
+      return false;
+      break;
+    case LS_DOWN:
+      LSD = record->event.pressed ? true : false;
+      leftVer = record->event.pressed ? 1  : (LSU ? -1 : 0);
+      leftStick();
+      return false;
+      break;
+    case LS_LEFT:
+      LSL = record->event.pressed ? true : false;
+      leftHor = record->event.pressed ? -1  : (LSR ? 1 : 0);
+      leftStick();
+      return false;
+      break;
+    case LS_RGHT:
+      LSR = record->event.pressed ? true : false;
+      leftHor = record->event.pressed ? 1  : (LSL ? -1 : 0);
+      leftStick();
+      return false;
+      break;
+    case RS_UPUP:
+      RSU = record->event.pressed ? true : false;
+      rightVer = record->event.pressed ? -1  : (RSD ? 1 : 0);
+      rightStick();
+      return false;
+      break;
+    case RS_DOWN:
+      RSD = record->event.pressed ? true : false;
+      rightVer = record->event.pressed ? 1  : (RSU ? -1 : 0);
+      rightStick();
+      return false;
+      break;
+    case RS_LEFT:
+      RSL = record->event.pressed ? true : false;
+      rightHor = record->event.pressed ? -1  : (RSR ? 1 : 0);
+      rightStick();
+      return false;
+      break;
+    case RS_RGHT:
+      RSR = record->event.pressed ? true : false;
+      rightHor = record->event.pressed ? 1  : (RSL ? -1 : 0);
+      rightStick();
+      return false;
+      break;
+    case DP_UPUP:
+      if(record->event.pressed){
+        DPU = true;
+        register_joystick_button(12);
+        unregister_joystick_button(13);
+      } else {
+        DPU = false;
+        unregister_joystick_button(12);
+        if (DPD) {
+          register_joystick_button(13);
+        }
+      }
+      return false;
+      break;
+    case DP_DOWN:
+      if(record->event.pressed){
+        DPD = true;
+        register_joystick_button(13);
+        unregister_joystick_button(12);
+      } else {
+        DPD = false;
+        unregister_joystick_button(13);
+        if (DPU) {
+          register_joystick_button(12);
+        }
+      }
+      return false;
+      break;
+    case DP_LEFT:
+      if(record->event.pressed){
+        DPL = true;
+        register_joystick_button(14);
+        unregister_joystick_button(15);
+      } else {
+        DPL = false;
+        unregister_joystick_button(14);
+        if (DPR) {
+          register_joystick_button(15);
+        }
+      }
+      return false;
+      break;
+    case DP_RGHT:
+      if(record->event.pressed){
+        DPR = true;
+        register_joystick_button(15);
+        unregister_joystick_button(14);
+      } else {
+        DPR = false;
+        unregister_joystick_button(15);
+        if (DPL) {
+          register_joystick_button(14);
+        }
       }
       return false;
       break;
@@ -470,10 +636,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 KC_SPC,KC_1,KC_LALT,KC_LALT,KC_1,  _______
   ),
   [_FIGHTER] = LAYOUT_split_3x5_3(
-    KC_TAB,   KC_Q,     AR_U,     KC_E,     _______,                      _______,  _______,  KC_B,     KC_X,     _______,
-    KC_LCTL,  AR_L,     AR_D,     AR_R,     _______,                      _______,  KC_Z,     KC_C,     KC_LSFT,  KC_V,
-    KC_ESC,   _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,
-                                  KC_Q,     KC_LSFT,  KC_E,     KC_ENT,   KC_SPC,   BASE
+    KC_TAB,   _______,  LS_UPUP,  _______,  _______,                      _______,  L2_PRSS,  L1_PRSS,  R1_PRSS,  R1_PRSS,
+    _______,  LS_LEFT,  LS_DOWN,  LS_RGHT,  LS_PRSS,                      RS_PRSS,  BC_UPUP,  BC_LEFT,  BC_RGHT,  BC_DOWN,
+    _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  GC_SLCT,  GC_STRT,
+                                  _______,  _______,  _______,  KC_ESC,  _______,  BASE
   )
   // clang-format on
 };
